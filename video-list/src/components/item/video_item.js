e5d8191a6b67009c0b21680b3e11cc37a9af1e44
malon <malonmiming@gmail.com>
2021-01-27 16:50:32 +0900

    list done

--- video-list/src/components/item/video_item.js
+++ video-list/src/components/item/video_item.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styles from './video_item.module.css';
 
 const VideoItem = ({ video: { snippet } }) =>
-  <li className={styles.video}>
-    <img className={styles.thumbnail} src={snippet.thumbnails.medium.url}
-     alt="video thumbnail"
-     />
-    <div>
-      <p className={styles.title}>{snippet.title}</p>
-      <p className={styles.channel}>{snippet.channelTitle}</p>
+
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img className={styles.thumbnail} src={snippet.thumbnails.medium.url}
+       alt="video thumbnail"
+       />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
     </div>
   </li>
 
--- video-list/src/components/list/video_list.js
+++ video-list/src/components/list/video_list.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import VideoItem from "../item/video_item";
+import VideoItem from '../item/video_item';
 import styles from './video_list.module.css';
 
+
 const VideoList = props => (
-  <ul>
+  <ul className={styles.videos}>
     {props.vidoes.map(video => (
       <VideoItem key={video.id} video={video} />
     ))}
